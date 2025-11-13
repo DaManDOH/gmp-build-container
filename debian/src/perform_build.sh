366219f6beb6a5d17e1e6c7533732913f9d5ec19
Dan C. Wlodarski <dan.wlodarski@gmail.com>
2025-11-13 03:06:09 +0000

    Added logic to build and test demo apps

--- debian/src/perform_build.sh
+++ debian/src/perform_build.sh
@@ -3,4 +3,13 @@ pushd ./gmp-6.3.0 >/dev/null
 ./configure
 make
 make check
+cd ./demos
+# Per https://gmplib.org/manual/Demonstration-Programs
+make pexpr factorize isprime primes qcn
+pushd ./calc >/dev/null
+make calc
+popd >/dev/null
+pushd ./expr
+make libexpr.a t-expr run-expr
+popd >/dev/null
 popd >/dev/null
